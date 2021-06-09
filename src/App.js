99f045ef05d40cdbf7ccc50232228afefe1f22f4
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2021-06-09 00:25:12 -0500

    I am having issues with async await currently the app is not fast the data I am retieving is not very useful

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { v4 as uuidv4 } from 'uuid';
 
+// env vars
 const client_id = process.env.REACT_APP_client_id;
 const client_secret = process.env.REACT_APP_client_secret;
 
@@ -11,17 +13,20 @@ export default class App extends Component {
     this.state = {
        latlong: '',
        searchInput:"",
+       rawData: [],
        venues: [],
-       venue_id: []
+       theVenue_id: [],
     }
   }
   
-  componentDidMount() { this.getLocation();}
-  getLocation = () => (navigator.geolocation.getCurrentPosition(response => this.setState({latlong: response.coords.latitude +","+response.coords.longitude}, () => this.getVenues(this.state.searchInput))));
-  
+  componentDidMount() {this.getLocation()}
+  // get users location
+  getLocation = () => (navigator.geolocation.getCurrentPosition(response => this.setState({latlong: response.coords.latitude +","+response.coords.longitude}, () => this.getVenues(this.state.searchInput), () => this.getVenueDetails())));
+  // get users text in state
   changeHandler = (event) => {
     this.setState({searchInput:event.target.value});
   }
+  // search for venues
   getVenues = (query) => {  
     const endPoint = "https://api.foursquare.com/v2/venues/search?";
     const params =  {
@@ -35,13 +40,12 @@ export default class App extends Component {
     };
 
 
-    
-    axios.get(endPoint + new URLSearchParams(params)).then(response => this.setState({venues: response.data.response.venues}));
+    axios.get(endPoint + new URLSearchParams(params)).then(response => this.setState({rawData: response.data.response.venues}));
+    axios.get(endPoint + new URLSearchParams(params)).then(response => this.setState({theVenue_id: response.data.response.venues.map(venue_id=>{return venue_id.id})}));
+    axios.get(endPoint + new URLSearchParams(params)).then(response => this.setState({venues: response.data.response.venues.map(venues=>{return venues.name})}));
+
   }
 
-  
-  
-  
   render() {
     return (
       <div>
@@ -50,10 +54,10 @@ export default class App extends Component {
           <button type="submit">Search</button>
         </form>
         <ul>
-          {/* {this.state.venues.map(venue=>{return <li key={venue.id}>{venue.name}</li>})} */}
-{this.state.venues.map(venue_id=>{return <li>{venue_id.id}</li>})
+          {this.state.venues.map(venue=>{return <li key={uuidv4()}>{venue}</li>})}
+          {/* {this.state.venues.map(venue_id=>{return <li>{venue_id.id}</li>})
       
-    }
+    } */}
 
       </ul>
       </div>
